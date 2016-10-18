5adf8929757b087dad41450716ea560bc3773965
jakevanhorn <jake.vanhorn13@gmail.com>
2016-10-18 09:44:45 -0700

    Added some base SFML code

--- Panzerkampfwagen/Panzerkampfwagen/Source.cpp
+++ Panzerkampfwagen/Panzerkampfwagen/Source.cpp
@@ -1,12 +1,44 @@
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
+using namespace sf;
 
 int main()
 {
-	cout << "Tanks Game!";
+	RectangleShape background(Vector2f(1400, 800));
+	background.setPosition(0, 0);
+	background.setFillColor(Color(204, 51, 77));
 
-	while (true){
+
+	RectangleShape ground(Vector2f(1400, 200));
+	ground.setPosition(0, 600);
+	ground.setFillColor(Color(51, 153, 204));
+
+	RenderWindow window(VideoMode(1400, 800), "Panzerkampfwagen");
+
+	while (window.isOpen()){
+
+		Event event1;
+		while (window.pollEvent(event1))
+		{
+			if (event1.type == Event::Closed){
+				window.close();
+			}
+		}
+
+		window.clear();
+
+		window.draw(background);
+		window.draw(ground);
+
+		window.display();
 
 	}
+
+	return 10;
 }
