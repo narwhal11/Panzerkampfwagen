a4409aecc76903a17379e8a0f0ee3d8e1dc6d584
jakevanhorn <jake.vanhorn13@gmail.com>
2016-10-20 10:32:33 -0700

    Added basic projectile motion

--- Panzerkampfwagen/Panzerkampfwagen/Source.cpp
+++ Panzerkampfwagen/Panzerkampfwagen/Source.cpp
@@ -10,6 +10,9 @@ using namespace sf;
 
 int main()
 {
+	Clock clock;
+	Time time1 = clock.getElapsedTime();
+
 	RectangleShape background(Vector2f(1400, 800));
 	background.setPosition(0, 0);
 	background.setFillColor(Color(204, 51, 77));
@@ -19,7 +22,19 @@ int main()
 	ground.setPosition(0, 600);
 	ground.setFillColor(Color(51, 153, 204));
 
+	double x_position = 10;
+	double y_position = 790;
+
+	RectangleShape projectile(Vector2f(10, 10));
+	projectile.setPosition(x_position, y_position);
+	projectile.setFillColor(Color(51, 153, 204));
+
+	double x_velocity = 180;
+	double y_velocity = -400;
+	double y_accel = 100;
+
 	RenderWindow window(VideoMode(1400, 800), "Panzerkampfwagen");
+	//window.setFramerateLimit(30);
 
 	while (window.isOpen()){
 
@@ -34,9 +49,19 @@ int main()
 		window.clear();
 
 		window.draw(background);
-		window.draw(ground);
+		//window.draw(ground);
+		window.draw(projectile);
 
 		window.display();
+
+		time1 = clock.getElapsedTime();
+		clock.restart();
+		x_position += (x_velocity * time1.asSeconds());
+		y_velocity += (y_accel * time1.asSeconds());
+		y_position += (y_velocity * time1.asSeconds());
+
+		projectile.setPosition(x_position, y_position);
+		//cout << x_position << " " << y_position << endl;
 
 	}
 
